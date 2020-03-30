c728cbdbf6025e189e512eaf0a6939bb2f4db337
WesleyLeocadio <silvawesley374@gmail.com>
2020-03-30 11:47:16 -0300

    Adicionando tests

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -7,17 +7,26 @@ module.exports = {
     connection: {
       filename: './src/database/db.sqlite'
     },
-    migrations:{
-      directory:'./src/database/migrations'
+    migrations: {
+      directory: './src/database/migrations'
     },
-    useNullAsDefault:true,
+    useNullAsDefault: true,
   },
-
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -33,7 +42,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
