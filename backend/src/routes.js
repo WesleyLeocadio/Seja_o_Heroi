b7feefb6509e8ec88645da46bed7ac4e2936b6ad
WesleyLeocadio <silvawesley374@gmail.com>
2020-03-30 10:32:38 -0300

    Adicionando validação com a biblioteca celebrate

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const { celebrate, Segments, Joi} = require('celebrate');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -8,15 +9,50 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/session', SessionController.create); 
+routes.post('/session',celebrate({
+    [Segments.BODY]:Joi.object().keys({
+        id: Joi.string().required(),
+    })
+}), SessionController.create); 
 
 routes.get('/ongs',OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs',celebrate({
+    [Segments.BODY]:Joi.object().keys({
+        name: Joi.string().required(),
+        email:Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf:Joi.string().required().length(2),
+    })
+}), OngController.create);
 
-routes.get('/profile',ProfileController.index)
+routes.get('/profile',celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index)
 
-routes.get('/incident',IncidentController.index);  
-routes.post('/incident', IncidentController.create);
-routes.delete('/incident/:id',IncidentController.delete);
+routes.get('/incident', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}),IncidentController.index);  
+
+routes.post('/incident',celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]:Joi.object().keys({
+        title: Joi.string().required(),
+        description:Joi.string().required(),
+        value: Joi.number().required()
+    })
+}),  IncidentController.create);
+
+routes.delete('/incident/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}),IncidentController.delete);
 
 module.exports = routes;
